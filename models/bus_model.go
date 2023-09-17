cdc61f4f3f79928cdc5bb540e394d8a27bbd1a6b
0Mayank <1mayanktomar@gmail.com>
2023-09-17 15:03:53 +0530

    [fix] missing field in bus model

--- models/bus_model.go
+++ models/bus_model.go
@@ -3,8 +3,9 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Bus struct {
-	Id     primitive.ObjectID `json:"_id"`
-	Plate  string             `json:"plate"  validate:"required"`
-	Number string             `json:"number" validate:"required"`
-	Route  []int              `json:"route"  validate:"required"`
+	Id      primitive.ObjectID `json:"_id"`
+	Plate   string             `json:"plate"  validate:"required"`
+	Number  string             `json:"number" validate:"required"`
+	BusType string             `json:"type"   validate:"required"`
+	Route   []int              `json:"route"  validate:"required"`
 }
