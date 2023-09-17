66c922e08f1e73cb3652a07660e0d7ebe3281ea6
0Mayank <1mayanktomar@gmail.com>
2023-09-17 14:39:41 +0530

    [chore] change route name

--- routes/bus_routes.go
+++ routes/bus_routes.go
@@ -6,7 +6,7 @@ import (
 )
 
 func BusRoutes(router *gin.Engine) {
-	router.POST("/bus/create", controllers.CreateBus())
+	router.POST("/bus/", controllers.CreateBus())
 	router.GET("/bus/search", controllers.SearchBus())
 	router.PUT("/bus/:busId", controllers.EditBus())
 	router.GET("/bus/:busId", controllers.GetBus())
