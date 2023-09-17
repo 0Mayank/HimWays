4b7cd06aa873db70c1c4b6b0e59a5f6cd5ba7111
0Mayank <1mayanktomar@gmail.com>
2023-09-17 15:34:04 +0530

    [fix] retardedness

--- models/bus_model.go
+++ models/bus_model.go
@@ -3,9 +3,9 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Bus struct {
-	Id      primitive.ObjectID `json:"_id"`
-	Plate   string             `json:"plate"  validate:"required"`
-	Number  string             `json:"number" validate:"required"`
-	BusType string             `json:"type"   validate:"required"`
-	Route   []int              `json:"route"  validate:"required"`
+	Id      primitive.ObjectID `bson:"_id"    json:"_id"`
+	Plate   string             `bson:"plate"  json:"plate"  validate:"required"`
+	Number  string             `bson:"number" json:"number" validate:"required"`
+	BusType string             `bson:"type"   json:"type"   validate:"required"`
+	Route   []int              `bson:"route"  json:"route"  validate:"required"`
 }
